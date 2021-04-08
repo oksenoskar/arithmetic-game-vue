6e0af95fa878e820360a06e898bc307ec15ba4b0
Oskar Eng <oskar_eng95@hotmail.com>
2021-04-08 17:14:51 +0200

    added images and animations

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,33 @@
 <template>
-  <div style="padding: 20px">
+
+  
+
+ <figure id="night-day">
+    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%">
+        <linearGradient id="skyGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <stop stop-color="rgb(0,0,12)" offset="0%" id="zenith">
+                <animate attributeName="stop-color" dur="60s" values="rgba(0,0,12,0);rgba(2,1,17,0);rgba(2,1,17,0);rgba(2,1,17,0);rgba(32,32,44,0.5);rgba(64,64,92,.8);rgb(74,73,105);rgb(117,122,191);rgb(130,173,219);rgb(148,197,248);rgb(183,234,255);rgb(144,223,254);rgb(87,193,235);rgb(45,145,194);rgb(36,115,171);rgb(30,82,142);rgb(30,82,142);rgb(21,66,119);rgba(22,60,82,0.8);rgba(7,27,38,.5);rgba(1,10,16,.3);rgba(9,4,1,0);rgba(0,0,12,0);rgba(0,0,12,0)" repeatCount="indefinite" />
+                <animate attributeName="offset" dur="24s" values="0;.85;.6;.1;0;0;0;0;0;.01;0;0;0;0;0;0;0;0;0;0;.3,.5,.8,0" repeatCount="indefinite" />
+            </stop>
+            <stop stop-color="rgb(0,0,12)" offset="100%" id="horizon">
+                <animate attributeName="stop-color" dur="60s" values="rgba(0,0,12,0);rgba(25,22,33,.3);rgba(32,32,44,.8);rgb(58,58,82);rgb(81,81,117);rgb(138,118,171);rgb(205,130,160);rgb(234,176,209);rgb(235,178,177);rgb(177,181,234);rgb(148,223,255);rgb(103,209,251);rgb(56,163,209);rgb(36,111,168);rgb(30,82,142);rgb(91,121,131);rgb(157,166,113);rgb(233,206,93);rgb(178,99,57);rgb(47,17,7);rgb(36,14,3);rgb(47,17,7);rgba(75,29,6,.4);rgba(21,8,0,0);rgba(0,0,12,0)" repeatCount="indefinite" />
+            </stop>
+        </linearGradient>
+        <rect id="sky" x="0" y="0" width="100%" height="100%" style="fill:url(#skyGradient)" />
+    </svg>
+    <div class="sun"></div>
+    <div class="moon"></div>
+</figure>
+
+
+
+
+
+
+
+
+
+  <div style="padding: 1px">
       <input
         class="input shadow shadow-box shadow-focus"
         ref="inputPassword"
@@ -67,7 +95,7 @@
 
   </div>
 
-  <div style="margin-top: -30px;
+  <div style="margin-top: -23px;
                     margin-bottom: 1px;
                     margin-right: 50px;
                     margin-left: 950px;"
@@ -86,6 +114,9 @@
 
 <script>
 
+
+
+
 export default {
   name: 'App',
   components: {
@@ -94,9 +125,9 @@ export default {
     return {
       password: null,
       input: null,
-      result: "Jeg er ikke kryptert enda.",
+      result: "I'm not Encrypted yet.",
       inputDekrypter: null,
-      resultDekryptert: "Jeg er ikke dekryptert enda."
+      resultDekryptert: "I'm not Decrypted yet."
     }
   },
   methods: {
@@ -118,6 +149,8 @@ export default {
     }
   }
 }
+
+ 
 </script>
 
 <style>
@@ -129,22 +162,20 @@ export default {
   font-size: 20px;
   color: 1px 1px 0 #fff;
   margin-top: 60px;
+  margin-bottom: 200px;
   letter-spacing: -0.2px;
 }
 
 
 
-bbody, html {
-  background-color:#EBECF0;
-}
+
 
 .button {
   background-color: #E6E7EE;
   border: 1px solid #d1d9e6;
   padding: 60px 35px;
   border-radius: 200px;
-  font-size: 20px;
-  
+  font-size: 20px;  
   outline:none;
 } 
 
@@ -171,7 +202,7 @@ bbody, html {
 body, p, input, select, textarea, button {
     font-family: 'Montserrat', sans-serif;
     letter-spacing: -0.2px;
-    font-size: 16px;
+    font-size: 15px;
     
 }
 
@@ -186,7 +217,7 @@ body, p, input, select, textarea, button {
   text-align: center;
 }
 .shadow-box {
-  margin-right: $ruler/2;
+  margin-right: 8px;
   box-shadow:  inset 2px 2px 5px #BABECC, inset -5px -5px 10px #FFF;
   width: 100%;
   box-sizing: border-box;
@@ -202,17 +233,92 @@ body, p, input, select, textarea, button {
 
 .input {
   width: 300px;
+  margin-top: 50px;
+  
+}
+
+body{
+  background-image: url("desert.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  
 }
 
 
-html::after{
-  content:'';
-	background: linear-gradient(21deg, #1beabd, #010b63);
-	height:4px;
-	width:100%;
-	position:absolute;
-	left:0;
-	top:0;
+#night-day{
+    background-color:#000;
+    background-image:url("big_dipper.jpg");
+    background-size:cover;
+    font-size:0;
+    height:400px;
+    margin: -10px;
+    margin-top: -60px;
+    overflow:hidden;
+    position:relative;
+    width:100vw;
+    box-shadow: 5px 10px 20px #888888;
+    
+
+
 }
+.sun{
+    animation:suntrack 60s infinite forwards linear,sunpulse 2s alternate infinite;
+    background:radial-gradient(ellipse at center, #f90 0%, #fc0 60%, #ff0 100%);
+    border-radius:100%;
+    bottom:-7vw;
+    height:15vw;
+    position:absolute;
+    right:7vw;
+    transform-origin:-28vw 7vw;
+    width:15vw;
+}
+.moon{
+    animation:moontrack 60s infinite backwards linear;
+    animation-delay:30s;
+    background-image:url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/moon8.png);
+    background-position:center;
+    background-repeat:no-repeat;
+    background-size:90% 90%;
+    border-radius:100%;
+    bottom:-7vw;
+    height:15vw;
+    opacity:.8;
+    position:absolute;
+    right:7vw;
+    transform-origin:-28vw 7vw;
+    width:15vw;
+}
+.paused{
+    -webkit-animation-play-state:paused;
+    animation-play-state:paused;
+}
+@keyframes suntrack{
+    from{
+        transform:rotate(.15turn);
+    }
+    to{
+        transform:rotate(-.85turn);
+    }
+}
+@keyframes moontrack{
+    from{
+        transform:rotate(.15turn);
+    }
+    to{
+        transform:rotate(-.85turn);
+    }
+}
+@keyframes sunpulse{
+    from{
+        box-shadow:0 0 100px rgba(255,255,0,.4),0 0 100px rgba(255,255,0,.4);
+    }
+    to{
+        box-shadow:0 0 50px rgba(255,255,0,.8),0 0 75px rgba(255,255,0,.8);
+    }
+}
+
+
+
+
 
 </style>
